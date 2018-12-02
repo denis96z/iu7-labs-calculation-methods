4bfef603c2417e42088e76f1edeb74ef204496fb
denis96z <zinoviev.denis@yandex.ru>
2018-12-02 17:33:34 +0300

    third step implemented

--- lab01/lab01.m
+++ lab01/lab01.m
@@ -3,13 +3,17 @@ function lab01()
   printf("Zero step:\n");
   disp(c);
   
-  c = firstStep(c);
+  c = decCols(c);
   printf("First step:\n");
   disp(c);
   
-  c = secondStep(c);
+  c = decRows(c);
   printf("Second step:\n");
   disp(c);
+  
+  cSIZ = getPrimarySIZ(c);
+  printf("Third step:\n");
+  disp(cSIZ);
 endfunction
 
 function c = readMatrix()
@@ -20,34 +24,60 @@ function c = readMatrix()
      3, 11,  9,  6,  6;
      8, 10, 11,  8,  7;
   ];
+  
+  [m, n] = size(c);
+  assert(m == n);
 endfunction
 
-function c = firstStep(c)
-  [m, n] = size(c); 
+function c = decCols(c)
+  [n, _] = size(c);
   for i = 1:n
     colMin = c(1,i);
-    for j = 2:m
+    for j = 2:n
       if c(j,i) < colMin
         colMin = c(j,i);
       endif
     endfor
-    for j = 1:m
+    for j = 1:n
       c(j,i) -= colMin;
     endfor
   endfor
 endfunction
 
-function c = secondStep(c)
-  [m, n] = size(c); 
-  for i = 1:m
-    strMin = c(i,1);
+function c = decRows(c)
+  [n, _] = size(c);
+  for i = 1:n
+    rowMin = c(i,1);
     for j = 2:n
-      if c(i,j) < strMin
-        strMin = c(i,j);
+      if c(i,j) < rowMin
+        rowMin = c(i,j);
       endif  
     endfor
     for j = 1:n
-      c(i,j) -= strMin;
+      c(i,j) -= rowMin;
+    endfor
+  endfor
+endfunction
+
+function cSIZ = getPrimarySIZ(c)
+  [n, _] = size(c);
+  cSIZ = zeros(n, n);
+  for i = 1:n
+    for j = 1:n
+      if c(j,i) == 0
+        rowHasZero = false;
+        
+        for k = 1:n
+          if cSIZ(j,k) == 1
+            rowHasZero = true;
+            break;
+          endif
+        endfor
+        
+        if !rowHasZero
+          cSIZ(j,i) = 1;
+        endif
+      endif
     endfor
   endfor
 endfunction
